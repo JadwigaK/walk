5a6188841650e6f7dc3b82f691f9146ba335d05a
Jakub Siembab <jsiembab@x-formation.com>
2017-02-19 23:12:34 +0100

    bootstrap started, navbar added with login/logout logic

--- src/main/webapp/static/js/main.js
+++ src/main/webapp/static/js/main.js
@@ -1,6 +1,10 @@
 $(document).ready(function () {
 
-    $('#msg').html("This is updated by jQuery");
+    //check if browser remembers last session
+    if ($.cookie("token")) {
+        $('#navLogin').css('display', 'none');
+        $('#navLogout').css('display', 'block');
+    }
 
     $('#loginform').submit(function (event) {
         /* stop form from submitting normally */
@@ -15,16 +19,52 @@ $(document).ready(function () {
             url: 'login', // the url where we want to POST
             data: JSON.stringify(data),
             dataType: 'json', // what type of data do we expect back from the server
-            contentType: "application/json"
+            contentType: "application/json",
+            success: function (response) {
+                //save token in cookie
+                $.cookie("token", response.token);
+                //remove errors just in case
+                $('#loginform').removeClass("has-error");
+                $('#loginError').remove();
+                //change login to logout in navbar
+                $('#navLogin').css('display', 'none');
+                $('#navLogout').css('display', 'block');
+                //quit modal
+                $('#loginmodal').modal('toggle');
+            },
+            error: function (request, status, error) {
+                $('#loginform').addClass("has-error");
+                $('#loginform').after('<p class="text-danger" id="loginError">' + request.responseText + '</p>');
+            }
         })
+        return false;
+    });
 
+    $("#navLogout").click(function () {
+        $.cookie("token", null);
+        //switch logout to login in navbar
+        $('#navLogout').css('display', 'none');
+        $('#navLogin').css('display', 'block');
+    });
+
+    $('#msg').html("This is updated by jQuery");
+
+    //just for simple interaction
+    $("#btn1").click(function () {
+
+        $.ajax({
+            type: 'GET', // define the type of HTTP verb we want to use (POST for our form)
+            url: '/api/walks', // the url where we want to POST
+            dataType: 'json', // what type of data do we expect back from the server
+            contentType: "application/json",
+            beforeSend: function (request) {
+                request.setRequestHeader("X-Authorization", getAuthHeader());
+            }
+        })
         // using the done promise callback
             .done(function (data) {
-                //save token in cookie
-                $.cookie("token", data.token);
+                alert("Data: " + data[0].name + "\nStatus: " + status);
             });
-
-        return false;
     });
 });
 
